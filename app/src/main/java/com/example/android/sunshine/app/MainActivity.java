476594a1f7d0861e6315da48c5f1da5b56ace631
jackSzm <jszmelter@gmail.com>
2016-04-16 12:18:18 +0100

    Clean up MainActivity

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2014 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
@@ -22,8 +7,6 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.example.android.sunshine.app.sync.SunshineSyncAdapter;
 
 public class MainActivity extends AppCompatActivity implements ForecastFragment.Callback {
 
@@ -41,20 +24,22 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
         if (findViewById(R.id.weather_detail_container) != null) {
             mTwoPane = true;
             if (savedInstanceState == null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.weather_detail_container, new DetailFragment(), DETAIL_FRAGMENT_TAG)
-                        .commit();
+                replaceDetailFragment(new DetailFragment());
             }
         } else {
             mTwoPane = false;
             setupActionBar();
         }
 
-        ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_forecast));
+        ForecastFragment forecastFragment = ((ForecastFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
+    }
 
-        SunshineSyncAdapter.initializeSyncAdapter(this);
+    private void replaceDetailFragment(DetailFragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.weather_detail_container, fragment, DETAIL_FRAGMENT_TAG)
+                .commit();
     }
 
     private void setupActionBar() {
@@ -77,7 +62,6 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -101,19 +85,22 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
     @Override
     public void onItemSelected(Uri contentUri) {
         if (mTwoPane) {
-            Bundle args = new Bundle();
-            args.putParcelable(DetailFragment.DETAIL_URI, contentUri);
+            DetailFragment fragment = buildNewDetailFragment(contentUri);
+            replaceDetailFragment(fragment);
 
-            DetailFragment fragment = new DetailFragment();
-            fragment.setArguments(args);
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.weather_detail_container, fragment, DETAIL_FRAGMENT_TAG)
-                    .commit();
         } else {
             Intent intent = new Intent(this, DetailActivity.class);
             intent.setData(contentUri);
             startActivity(intent);
         }
+    }
+
+    private DetailFragment buildNewDetailFragment(Uri contentUri) {
+        Bundle args = new Bundle();
+        args.putParcelable(DetailFragment.DETAIL_URI, contentUri);
+
+        DetailFragment fragment = new DetailFragment();
+        fragment.setArguments(args);
+        return fragment;
     }
 }
